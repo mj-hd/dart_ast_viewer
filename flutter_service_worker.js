5fcfaacfb1774ca71efb62da05c910811e56498e
mj-hd <mj-hd@users.noreply.github.com>
2021-09-23 05:53:39 +0000

    deploy: 73cff86db61d211dff1c0b5a04391028529bc050

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"index.html": "92847c80aa234f3d172f3551c8f4cec7",
-"/": "92847c80aa234f3d172f3551c8f4cec7",
+"index.html": "e2ae495b3e9acb78c9ca676cc486f80b",
+"/": "e2ae495b3e9acb78c9ca676cc486f80b",
 "assets/NOTICES": "8315ec062a3a76eb50388e23077cd6c6",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
